08611211861d4f348634b8065f713aa3870c1a1e
Marko Haapasaari <marko1927@student.hamk.fi>
2023-05-02 17:06:40 +0300

    Artists controller base added

---
+++ Lippukauppa.fi/Controllers/ArtistsController.cs
@@ -0,0 +1,21 @@
+﻿using Lippukauppa.fi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lippukauppa.fi.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ArtistsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Artists.ToList();
+            return View(data);
+        }
+    }
+}
