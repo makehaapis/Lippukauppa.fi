bffcbd3ee2cd3b78031217f5a20532e9ad811209
Marko Haapasaari <marko1927@student.hamk.fi>
2023-05-02 13:01:25 +0300

    Artist model added

---
+++ Lippukauppa.fi/Models/Artist.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Lippukauppa.fi.Models
+{
+    public class Artist
+    {
+        [Key]
+        public int ArtistId { get; set; }
+
+        [Required(ErrorMessage = "Title is required!")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 50 characters")]
+        public string? Title { get; set; }
+
+        [Display(Name = "Profile Picture URL")]
+        [Required(ErrorMessage = "Profile Picture is required ")]
+        public string? ProfilePictureURL { get; set; }
+
+        [StringLength(1000)]
+        public string? Description { get; set; }
+
+        //Relationships
+    }
+}
