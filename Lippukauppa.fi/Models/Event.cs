1f7c75174f2c1c1c092bbaa72e2f64fb8750a752
Marko Haapasaari <marko1927@student.hamk.fi>
2023-05-02 13:32:08 +0300

    Event model added

---
+++ Lippukauppa.fi/Models/Event.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lippukauppa.fi.Models
+{
+    public class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double TicketPrice { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public DateTime SellStartDate { get; set; }
+        public int TicketQuantity { get; set; }
+
+        //Relationships
+        //public List<Artist_Event> Artists_Events { get; set; }
+        //Venue
+        public int VenueId { get; set; }
+        [ForeignKey("VenueId")]
+
+        public Venue Venue { get; set; }
+    }
+}
