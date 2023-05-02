f0ffc941a61415ef30167013de299793bbcc681c
Marko Haapasaari <marko1927@student.hamk.fi>
2023-05-02 12:35:13 +0300

    Add project files.

---
+++ Lippukauppa.fi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lippukauppa.fi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
