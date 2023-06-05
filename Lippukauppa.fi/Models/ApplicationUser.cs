481115bbf4361e01f3f8c396665d3cfc41bd4459
Marko Haapasaari <marko1927@student.hamk.fi>
2023-06-05 10:19:09 +0300

    paypal integration added

---
+++ Lippukauppa.fi/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lippukauppa.fi.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+    }
+}
